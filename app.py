5178b58b9f4981ffada3dc7ba466dfea936d6d74
Britt van Gemert <britt.van.gemert@politie.nl>
2024-10-21 15:28:09 +0200

    Add st auth

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.pages import excel
 import streamlit as st
 from streamlit_option_menu import option_menu
 from datetime import datetime
+import hmac
 
 st.set_page_config(
     page_title="Planning tool", page_icon=":calendar:", layout="centered"
@@ -40,30 +41,36 @@ with st.sidebar:
 with open("src/style/style.css", encoding="utf-8") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-# The correct password
-correct_password = open("auth.txt").read().strip()
 
-# Initialize session state for password check
-if "authenticated" not in st.session_state:
-    st.session_state.authenticated = False
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Voer wachtwoord in",
+        type="password",
+        on_change=password_entered,
+        key="password",
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Onjuist wachtwoord")
+    return False
 
 
-@st.dialog("Voer wachtwoord in")
-def password_dialog():
-    with st.form(key="password_form"):
-        password_input = st.text_input("Wachtwoord", type="password")
-        # Submit button inside the form
-        submit_button = st.form_submit_button("Submit")
-        if submit_button:
-            if password_input == correct_password:
-                st.session_state.authenticated = True
-                st.rerun()  # Refresh the app state
-            else:
-                st.error("Onjuist wachtwoord.")
-
-
-if st.session_state.authenticated == False:
-    password_dialog()
+if not check_password():
+    st.stop()
 
 #########################
 ######### PAGES #########
@@ -71,14 +78,8 @@ if st.session_state.authenticated == False:
 
 if choice == f"Week {datetime.today().isocalendar()[1]} - Dev":
     st.title(f":blue[Week {datetime.today().isocalendar()[1]} - Dev]")
-    if st.session_state.authenticated:
-        excel.show_excel("src/data/data_planning_dev.csv")
-    else:
-        st.warning("You need to be authenticated to view this page.")
+    excel.show_excel("src/data/data_planning_dev.csv")
 
 if choice == f"Week {datetime.today().isocalendar()[1]} - Support - Exposure":
     st.title(f":orange[Week {datetime.today().isocalendar()[1]} - Support - Exposure]")
-    if st.session_state.authenticated:
-        excel.show_excel("src/data/data_planning_support.csv")
-    else:
-        st.warning("You need to be authenticated to view this page.")
+    excel.show_excel("src/data/data_planning_support.csv")
